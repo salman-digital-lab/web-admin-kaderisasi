e40d2365abeb04f7cdce65c5b2cea7383a655c91
naufalzhafran <latifnaufal7@gmail.com>
2021-07-31 13:55:02 +0700

    add absolute import

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import "../../assets/scss/Dashboard.scss"
+import "assets/scss/Dashboard.scss"
 import TableStatistic from "./component/tabel-statistic"
 import Card from "./component/index-card"
-import { AdminDashboardProvider } from "../../context/AdminDashboardContext"
-import { AdminContext } from "../../context/AdminContext"
+import { AdminDashboardProvider } from "context/AdminDashboardContext"
+import { AdminContext } from "context/AdminContext"
 
 //Function Pemanggilan Component Card Statistic dan Table Statistic
 
