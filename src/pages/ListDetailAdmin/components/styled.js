c4cead890b6e453f2c3e23645046174b1415f55d
Leja Aprianza <lejaprianza@gmail.com>
2021-07-31 06:12:44 +0000

    Origin/admin/detail admin2

---
+++ src/pages/ListDetailAdmin/components/styled.js
@@ -0,0 +1,41 @@
+import { makeStyles } from "@material-ui/core";
+export const styled = makeStyles((theme) => ({
+  root: {
+    // height: 400,
+    flexGrow: 1,
+    // minWidth: 300,
+    transform: "translateZ(0)",
+    // The position fixed scoping doesn't work in IE 11.
+    // Disable this demo to preserve the others.
+    "@media all and (-ms-high-contrast: none)": {
+      display: "none",
+    },
+  },
+  modal: {
+    display: "flex",
+    padding: theme.spacing(1),
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    width: "30%",
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+  table: {
+    minWidth: "100%",
+    border: "none",
+  },
+  cell: {
+    borderBottom: "none",
+    paddingBottom: 4,
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+  CloseAndSubmit: {
+    borderBottom: "none",
+    paddingLeft: 0,
+    paddingRight: 0,
+  },
+}));
