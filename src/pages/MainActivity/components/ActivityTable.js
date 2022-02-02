fec507c3832e3a94c025d75015d80c58d960b583
Himiko <fahriramadhan@student.untan.ac.id>
2022-02-02 17:38:01 +0700

    update

--- src/pages/MainActivity/components/ActivityTable.js
+++ src/pages/MainActivity/components/ActivityTable.js
@@ -34,10 +34,6 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: "100%",
   },
-  paper: {
-    width: "100%",
-    marginBottom: theme.spacing(2),
-  },
   table: {
     minWidth: 750,
   },
