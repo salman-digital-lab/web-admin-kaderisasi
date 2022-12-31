382afd91767aed9148047b21cde86f978f2426be
Muhammad Iqbal <iqbal.adudu@gmail.com>
2022-12-31 15:43:21 +0200

    lmd spectra ssc card done

--- src/pages/Dashboard/component/index-card.js
+++ src/pages/Dashboard/component/index-card.js
@@ -1,9 +1,16 @@
 import React, { useContext, useEffect } from "react"
 import CardDashboard from "./CardDashboard"
 import { AdminDashboardContext } from "../../../context/AdminDashboardContext"
+import { useTheme } from "@mui/material/styles"
+import useMediaQuery from "@mui/material/useMediaQuery"
 import { Grid } from "@material-ui/core"
 
 const Card = () => {
+  const theme = useTheme()
+  const xs = useMediaQuery(theme.breakpoints.between("xs", "sm"))
+  const sm = useMediaQuery(theme.breakpoints.between("sm", "md"))
+  const md = useMediaQuery(theme.breakpoints.between("md", "lg"))
+  const lg = useMediaQuery(theme.breakpoints.up("lg"))
   const { valueMapping, functions } = useContext(AdminDashboardContext)
   const { CardData, GetAllMember } = functions
   const data = [...CardData]
@@ -12,6 +19,10 @@ const Card = () => {
       GetAllMember()
     }
   })
+
+  const getWidthValues = () => {
+    return (xs && "50%") || (sm && "40%") || (md && "40%") || (lg && "25%")
+  }
 
   return (
     <Grid
@@ -25,7 +36,16 @@ const Card = () => {
       }}
     >
       {data.map((e) => (
-        <Grid item xs={12} md={6} lg={3} key={`${e.value}_${e.title}`}>
+        <Grid
+          item
+          xs
+          md
+          lg
+          key={`${e.value}_${e.title}`}
+          style={{
+            minWidth: getWidthValues(),
+          }}
+        >
           <CardDashboard
             color={e.color}
             icon={e.icon}
